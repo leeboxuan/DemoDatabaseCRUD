783216dfc0ed02cdcb54a655ff43c28638fa0b06
16046473 <leeboxuan@hotmail.com>
2018-05-21 16:59:10 +0800

    Initial commit

---
+++ app/src/main/java/com/weebly/leeboxuan/demodatabasecrud/EditActivity.java
@@ -0,0 +1,73 @@
+package com.weebly.leeboxuan.demodatabasecrud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+int code = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        //initialize the variables with UI here
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        tvID = (TextView) findViewById(R.id.tvID);
+        etContent = (EditText) findViewById(R.id.etContent);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                Toast.makeText(EditActivity.this, "Update successful",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.putExtra("type", "update");
+                setResult(RESULT_OK, intent);
+                finish();
+
+                dbh.close();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                Toast.makeText(EditActivity.this, "Delete successful",
+                        Toast.LENGTH_SHORT).show();
+Intent intent = new Intent();
+intent.putExtra("type", "delete");
+setResult(RESULT_OK, intent);
+                                finish();
+
+
+                dbh.close();
+            }
+        });
+
+
+
+
+    }
+}
